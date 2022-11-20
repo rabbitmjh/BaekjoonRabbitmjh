a5956588ac78fe9bbd68a7b8535321938e528654
Min Ji Hong <rabbitmjh@naver.com>
2022-11-20 20:18:19 +0900

    [Bronze V] Title: 새싹, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.cc
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<int, p> pp;
+
+int main() {
+	FIO;
+
+	cout << "         ,r'\"7\n";
+	cout << "r`-_   ,'  ,/\n";
+	cout << " \\. \". L_r'\n";
+	cout << "   `~\\/\n";
+	cout << "      |\n";
+	cout << "      |\n";
+	return 0;
+}
