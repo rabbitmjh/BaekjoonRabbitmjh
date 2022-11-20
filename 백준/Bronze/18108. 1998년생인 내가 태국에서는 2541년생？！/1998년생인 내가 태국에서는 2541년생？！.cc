2a9c28d5f43894fd22d1e1df2fc426d3ba63e6bd
Min Ji Hong <rabbitmjh@naver.com>
2022-11-20 18:08:54 +0900

    [Bronze V] Title: 1998년생인 내가 태국에서는 2541년생?!, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/18108. 1998년생인 내가 태국에서는 2541년생？！/1998년생인 내가 태국에서는 2541년생？！.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<int, p> pp;
+
+int y;
+
+int main() {
+	FIO;
+
+	cin >> y;
+	cout << y - 543;
+	return 0;
+}
