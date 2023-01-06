e6ebc607a8cf7b490dd7e7b63975fb309b4e5739
Min Ji Hong <rabbitmjh@naver.com>
2023-01-06 19:37:02 +0900

    [Bronze V] Title: 꼬마 정민, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ 백준/Bronze/11382. 꼬마 정민/꼬마 정민.cc
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+#define FIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
+typedef long long ll;
+typedef pair<int, int> p;
+typedef pair<int, p> pp;
+
+ll a, b, c;
+
+int main() {
+	FIO;
+	cin >> a >> b >> c;
+	cout << a + b + c;
+
+	return 0;
+}
