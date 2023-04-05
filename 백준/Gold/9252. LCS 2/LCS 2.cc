c66c15bb1bf18b33b9e39624dcabe89eb9a60be4
Min Ji Hong <rabbitmjh@naver.com>
2023-04-05 19:04:10 +0900

    [Gold IV] Title: LCS 2, Time: 4 ms, Memory: 5972 KB -BaekjoonHub

--- 백준/Gold/9252. LCS 2/LCS 2.cc
+++ 백준/Gold/9252. LCS 2/LCS 2.cc
@@ -1,16 +1,6 @@
-#include <iostream>
-#include <algorithm>
-#include <functional>
-#include <queue>
-#include <vector>
-#include <utility>
-#include <deque>
-#include <string>
-#include <limits>
-#include <cmath>
-#include <cstring>
-#include <array>
+#include <bits/stdc++.h>
 using namespace std;
+#define FIO ios::sync_with_stdio(false), cin.tie(NULL), cout.tie(NULL);
 typedef long long ll;
 typedef pair<int, int> p;
 typedef pair<int, p> pp;
@@ -20,11 +10,10 @@ int arr[1005][1005], ans;
 vector <char> s;
 
 int main() {
-	ios::sync_with_stdio(false);
-	cin.tie(NULL);
-
+	FIO;
 	cin >> s1 >> s2;
 	swap(s1, s2);
+    
 	for (int i = 0; i < s1.size(); i++) {
 		int cnt = 0;
 		for (int j = 0; j < s2.size(); j++) {
